a960e7d58855f3015911a0cad5d46fd134a120d4
Rohanth S R <156525984+rohanth18@users.noreply.github.com>
2026-02-04 23:20:03 +0530

    Add files via upload
    A Python program that uses the YOLOv8 model and OpenCV to perform real-time object detection through a webcam, displaying detected objects with bounding boxes.

---
+++ computer_vision_using_yolo.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+import cv2
+
+model=YOLO('yolov8n.pt')
+
+cap=cv2.VideoCapture(0)
+
+while True:
+    ret,frame=cap.read()
+
+    results=model(frame)
+
+    annotated_frame=results[0].plot()
+
+    cv2.imshow('YOLOv8 Detection', annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
